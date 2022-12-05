a4f509fef8e5bfe7fea622474ecbacc4c274c168
miyotake <113184420+miyotake@users.noreply.github.com>
2022-12-05 15:48:37 +0900

    削除機能修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,17 +1,17 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
-  before_action :set_prototype, except: [:index, :new, :create]
+  before_action :move_to_index, except: [:index,:show]
+  before_action :edit_move_to_index, only: [:edit]
+  before_action :set_prototype, only: [:edit, :show, :update]
+   
+  
 
   def index
     @prototypes = Prototype.all
   end
 
-  def edit
-  end
-
   def new
     @prototype = Prototype.new
+    authenticate_user!
   end
 
   def create
@@ -19,43 +19,56 @@ class PrototypesController < ApplicationController
     if @prototype.save
       redirect_to root_path
     else
-      render :new
+      render action: :new
     end
   end
 
   def show
     @comment = Comment.new
     @comments = @prototype.comments.includes(:user)
+    
+  end
+
+  def edit
+    authenticate_user!
   end
 
   def update
     if @prototype.update(prototype_params)
-       redirect_to prototype_path(@prototype)
+      redirect_to prototype_path
     else
       render :edit
     end
   end
 
   def destroy
-    if @prototype.destroy
-      redirect_to root_path
-    else
-      redirect_to root_path
-    end
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
   end
+
 
   private
 
   def prototype_params
-    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+    params.require(:prototype).permit(:image, :title, :catch_copy, :concept).merge(user_id: current_user.id)
   end
 
   def set_prototype
     @prototype = Prototype.find(params[:id])
   end
 
-  def contributor_confirmation
-    @prototype = Prototype.find(params[:id])
-    redirect_to root_path unless current_user == @prototype.user
+  def move_to_index
+    unless user_signed_in? 
+      authenticate_user!
+    end
   end
+
+  def edit_move_to_index
+    @prototype_id = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype_id.user_id 
+      redirect_to action: :index
+    end
+  end
+
 end
