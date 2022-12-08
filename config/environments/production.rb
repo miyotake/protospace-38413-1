3d0ee9d65c3f7b73b5c1131b297926a76ce87793
miyotake <113184420+miyotake@users.noreply.github.com>
2022-12-08 16:30:59 +0900

    本番環境に記述

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts << "protospace-38413-1.onrender.com"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
